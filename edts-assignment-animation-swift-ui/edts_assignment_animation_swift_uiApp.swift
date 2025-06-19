45b3a55bd8aafe16e21bc86580e8ef664203d715
Fauzan Sukmapratama <fauzansukmapratama@upi.edu>
2025-06-19 10:43:24 +0700

    Initial Commit

---
+++ edts-assignment-animation-swift-ui/edts_assignment_animation_swift_uiApp.swift
@@ -0,0 +1,17 @@
+//
+//  edts_assignment_animation_swift_uiApp.swift
+//  edts-assignment-animation-swift-ui
+//
+//  Created by Fauzan Sukmapratama on 19/06/25.
+//
+
+import SwiftUI
+
+@main
+struct edts_assignment_animation_swift_uiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
